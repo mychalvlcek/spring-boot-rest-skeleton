04a0a9ad0068000971262a59753f7297925f4f68
Michal Vlcek <mychalvlcek@gmail.com>
2016-12-05 23:17:13 +0100

    spring-boot init

---
+++ README.md
@@ -0,0 +1,8 @@
+# spring-boot rest skeleton
+
+#### # installation
+ * setup `src/main/resources/application-dev.properties` from template (`src/main/resources/application-dev.template.properties`)
+ * run `SPRING_PROFILES_ACTIVE=dev,default mvn spring-boot:run`
+
+#### # supported technologies
+ * database migrations (https://flywaydb.org/)
---
+++ src/main/java/com/example/config/DataSourceConfiguration.java
@@ -0,0 +1,19 @@
+package com.example.config;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfiguration {
+
+	@Bean
+	@ConfigurationProperties(prefix = "spring.datasource")
+	public DataSource primaryDataSource() {
+		DataSource dataSource = DataSourceBuilder.create().build();
+		return dataSource;
+	}
+}
